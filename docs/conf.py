8353c7f4433095bf953dc7104ed0f45c4e9b76a1
Somiyana <somiyanamir@gmail.com>
2025-06-19 15:58:34 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'HP Color LaserJet Pro M255dw Printer'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
